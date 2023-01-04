ba9f168706836e64126af5da56e8b676a2104963
Valeria Serci <balerty84@gmail.com>
2023-01-04 18:18:06 +0100

    fill arrays

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-let a = [];
-let b = [];
-let c = [];
+let a = ['You will', 'You will not', 'You may', 'You might'];
+let b = ['get married', 'have a child', 'lose your job', 'get a new job', 'win the lottery'];
+let c = ['this year', 'next year', 'soon', 'in 2 years from now'];
 
 const ranEl = elements => {
     return elements[Math.floor(Math.random() * elements.length)]
 }
+
