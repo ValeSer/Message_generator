0bf827a66c84ab5a8c955ac07c9746c841f33f20
Valeria Serci <balerty84@gmail.com>
2023-01-04 19:33:37 +0100

    update array c

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let a = ['You will', 'You will not', 'You may', 'You might'];
 let b = ['get married', 'have a child', 'lose your job', 'get a new job', 'win the lottery'];
-let c = ['this year', 'next year', 'soon', 'in 2 years from now'];
+let c = ['this year!', 'next year!', 'soon!', 'in 2 years from now!'];
 
 const ranEl = elements => {
     return elements[Math.floor(Math.random() * elements.length)]
